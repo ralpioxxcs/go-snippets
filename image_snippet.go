adbf09ca90aca73f8492e9850aedcd7ab8071e26
ralpioxxcs <tmfmfkal1123@naver.com>
2022-05-03 17:04:18 +0900

    download image

--- image_snippet.go
+++ image_snippet.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	fmt.Println("vim-go")
+}
+
+func downloadFromUrl(url string) {
+	resource, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resource.Body.Close()
+
+	file, err := os.Create("image.jpg")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	file.ReadFrom(resource.Body)
 }
